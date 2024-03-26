5e06a410a53c03406950a80e830e74e3d17d6e0a
Emelie Skoog <emelie.skoog@hiq.se>
2024-03-26 15:14:09 +0100

    Update client

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,24 +1,19 @@
 import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
 import { PortableTextBlock, PortableTextChild } from "sanity"
+import clientConfig from './config/client-config'
 
 export async function getProjects(): Promise<Project[]> {
-    const client = createClient({
-        projectId: 'kko74km7',
-        dataset: 'production',
-        apiVersion: '2024-02-09',
-    })
-
-    return client.fetch(
-        groq`*[_type=="project"]{
-            _id,
-            _createdAt,
-            name,
-            "slug":slug.current,
-            "image":image.asset->url,
-            url,
-            content
-        }`
+    return createClient(clientConfig).fetch(
+        groq`*[_type == "project"]{
+        _id,
+        _createdAt,
+        name,
+        "slug": slug.current,
+        "image": image.asset->url,
+        url,
+        content
+      }`
     )
 }
 
